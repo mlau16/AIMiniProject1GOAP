e2a9158f324dde523d2c5400576fbbf0d1b9dce1
Unknown <mlau16@student.aau.dk>
2019-02-28 15:12:31 +0100

    Ping pong hello world server

--- GOAP_PythonFiles/GOAPServer.py
+++ GOAP_PythonFiles/GOAPServer.py
@@ -4,24 +4,14 @@ import cv2
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5500")
-
+socket.bind("tcp://*:5555")
+print("bound socket to 5555")
 while True:
     #  Wait for next request from client
+    print("Waiting to recieve message")
     message = socket.recv()
-
     time.sleep(0.1)  # Small delay to wait for the texture to be made
-
     print("server ready to recieve data")
+    socket.send(message)
+    print("Sent back message")
 
-    print("server wrote to image")
-    socket.send(b"pong")
-
-
-    #  In the real world usage, you just need to replace time.sleep() with
-    #  whatever work you want python to do.
-    time.sleep(1)
-
-    #  Send reply back to client
-    #  In the real world usage, after you finish your work, send your output here
-    socket.send(b"World")
