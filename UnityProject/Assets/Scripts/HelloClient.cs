907b9d5a37f662b4ba9e75e40382c3eb52fb3915
Unknown <mlau16@student.aau.dk>
2019-02-28 15:11:46 +0100

    ping pong hello world

--- UnityProject/Assets/Scripts/HelloClient.cs
+++ UnityProject/Assets/Scripts/HelloClient.cs
@@ -5,6 +5,7 @@ using NetMQ.Sockets;
 using UnityEngine;
 using System.Collections;
 using System.IO;
+using System.Text;
 
 public class HelloClient : MonoBehaviour
 {
@@ -17,22 +18,9 @@ public class HelloClient : MonoBehaviour
 
         if (SendPack)
         {
-            //Create a new texture with the width and height of the screen
-            Texture2D texture = new Texture2D(256, 256, TextureFormat.RGB24, false);
-            //Read the pixels in the Rect starting at 0,0 and ending at the screen's width and height
-            texture.ReadPixels(new Rect(0, 0, 256, 256), 0, 0, false);
-            texture.Apply();
-            //Byte encode the image
-            current_img = texture.EncodeToPNG();
-            //Destroy the tex for mem leak control
-            Object.Destroy(texture);
-
-            // For testing purposes, also write to a file in the project folder, if you want to save the image directly from Unity, use this.
-            //File.WriteAllBytes(Application.dataPath + "SavedScreen.png", current_img);
-
-            _helloRequester.bytes = current_img;
+            string hey = "Hello world";
+            _helloRequester.bytes = Encoding.ASCII.GetBytes(hey);
             _helloRequester.Continue();
-            Debug.Log("Sent image data");
         }
         else if (!SendPack)
         {
